4be722bd8b6f5699e72bf71b96ec4a180a546819
Aanandakrishnan D <aanand@kissflow.com>
2024-02-16 12:14:14 +0530

    [Ak] Added suppliern response UI

--- src/pages/SupplierResponsePage.tsx
+++ src/pages/SupplierResponsePage.tsx
@@ -1,13 +1,25 @@
 import type { CollapseProps } from 'antd';
-import { Col, Collapse, Row, Typography, theme } from 'antd';
+import { Col, Collapse, DatePicker, Input, Row, Select, Typography, theme } from 'antd';
 import TextArea from 'antd/es/input/TextArea';
 import type { CSSProperties } from 'react';
-import React from 'react';
+import React, { useState } from 'react';
 import { lightGrey } from '../helpers/colors';
 
 const text = `
 Design Parameters & Constraints
 `;
+
+
+interface Props {
+    type: string;
+    options?: string;
+}
+
+interface NewProps {
+    value: any;
+    setValue: (value: any) => void;
+}
+
 
 const getItems: (panelStyle: CSSProperties) => CollapseProps['items'] = (panelStyle) => [
     {
@@ -30,6 +42,29 @@ const getItems: (panelStyle: CSSProperties) => CollapseProps['items'] = (panelSt
         style: panelStyle,
     },
 ];
+
+const sampleData = [
+    {
+        type: "single_select",
+        options: JSON.stringify([{
+            key: "1",
+            value: "Hello"
+        },
+        {
+            key: "2",
+            value: "How"
+        }])
+    },
+    {
+        type: "short_text"
+    },
+    {
+        type: "date_time"
+    },
+    {
+        type: "text_area"
+    }
+]
 
 const SupplierResponsePage: React.FC = () => {
     const { token } = theme.useToken();
@@ -62,55 +97,120 @@ const SupplierResponsePage: React.FC = () => {
 
 function Questionnaire() {
     return (
-        <div style={{ 
-            borderTop: "1px solid #D8DCE5", 
-            backgroundColor: "white", 
-            padding: 20,
+        <div style={{
+            borderTop: "1px solid #D8DCE5",
+            backgroundColor: "white"
             // margin: 3
-            }} >
-            <div style={{ marginTop: 10 }} >
-                <Typography style={{ fontSize: 15, color: lightGrey, fontWeight: "400" }} >
+        }} >
+            <div style={{ marginTop: 5 }} >
+                <Typography style={{ fontSize: 15, color: lightGrey, fontWeight: "400", margin: 20 }} >
                     {text}
                 </Typography>
-                <div style={{paddingTop: 10}} >
-                    <Row style={{marginTop:15}} >
-                        <Col span={3}  >
-                            <Typography style={{fontWeight:"600", fontSize: 14}} >
-                                QUESTION
-                            </Typography>
-                        </Col>
-                        <Col span={21} >
-                            <Typography>
-                                What are the quality control procedures and standards in place for the manufacturing of the product?
-                            </Typography>
-                        </Col>
-                    </Row>
-                    <Row style={{marginTop:15}} >
-                        <Col span={3}  >
-                            <Typography style={{fontWeight:"600", fontSize: 14}} >
-                                ANSWER
-                            </Typography>
-                        </Col>
-                        <Col span={21} >
-                            <Typography>
-                                <TextArea placeholder='Enter your Answer here' ></TextArea>
-                            </Typography>
-                        </Col>
-                    </Row>
-                </div>
+                {
+                    sampleData.map((data, index) => (
+                        <div>
+                            <div style={{ margin: 20 }} >
+                                <Inputs {...data} />
+                            </div>
+                            {sampleData.length-1 > index &&
+                                <div style={{ borderBottom: "1px solid #D8DCE5", marginTop: 50 }} ></div>}
+                        </div>
+                    ))
+                }
             </div>
         </div>
     )
 }
-interface Props {
-    type: string;
-    options?: string;
+
+function Inputs({ type, options }: Props) {
+    const [value, setValue] = useState<any>()
+    return (
+        <div style={{ paddingTop: 10 }} >
+            <Row style={{ marginTop: 15 }} >
+                <Col span={3}  >
+                    <Typography style={{ fontWeight: 600, fontSize: 14 }} >
+                        QUESTION
+                    </Typography>
+                </Col>
+                <Col span={21} >
+                    <Typography style={{ fontSize: 14, fontWeight: 500 }} >
+                        What are the quality control procedures and standards in place for the manufacturing of the product?
+                    </Typography>
+                </Col>
+            </Row>
+            <Row style={{ marginTop: 15 }} >
+                <Col span={3}  >
+                    <Typography style={{ fontWeight: "600", fontSize: 14 }} >
+                        ANSWER
+                    </Typography>
+                </Col>
+                <Col span={21} >
+                    {/* <Typography>
+                        <TextArea placeholder='Enter your Answer here' ></TextArea>
+                    </Typography> */}
+                    <GetField type={type} options={options} value={value} setValue={setValue} />
+                </Col>
+            </Row>
+        </div>
+    )
 }
-// export function GetField({type, options} :Props) {
-//     switch(type) {
-//         case ""
-//     }
-// }
+
+export function GetField({ type, options, value, setValue }: Props & NewProps) {
+    const parsedOptions: any[] = JSON.parse(options ? options : "[]");
+    function onChange(event: any, dateString?: any) {
+        if (type == "single_select") {
+            setValue(event)
+        } else if (type == "date_time") {
+            setValue(dateString)
+        }
+        else {
+            setValue(event.target.value);
+        }
+    }
+
+    switch (type) {
+        case "short_text":
+            return (
+                <Input
+                    placeholder='Enter your answer here'
+                    style={{ height: 40 }}
+                    onChange={onChange}
+                />
+            )
+        case "text_area":
+            return (
+                <TextArea
+                    placeholder='Enter your answer here'
+                    style={{ minHeight: 106 }}
+                    onChange={onChange}
+                />
+            )
+        case "single_select":
+            return (
+                <Select
+                    options={parsedOptions}
+                    onChange={onChange}
+                    placeholder='Choose your answer here'
+                    style={{ height: 40, width: 300 }}
+                />
+            )
+        case "date_time":
+            return (
+                <DatePicker
+                    placeholder='Enter your answer here'
+                    onChange={onChange}
+                    showTime
+                    style={{ height: 40, width: 300 }}
+                />
+            )
+        default:
+            return (
+                <div>
+                    Something went wrong.
+                </div>
+            )
+    }
+}
 
 export {
     SupplierResponsePage
