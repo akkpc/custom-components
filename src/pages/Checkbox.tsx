f9b31529aa96af670be2ce08b60144b49a5c15d8
Aanandakrishnan D <aanand@kissflow.com>
2024-03-02 19:22:27 +0530

    [Ak] Added t and c condition tab

--- src/pages/Checkbox.tsx
+++ src/pages/Checkbox.tsx
@@ -1,5 +1,6 @@
-import { Checkbox } from 'antd'
-import React, { useEffect, useState } from 'react'
+import { Checkbox, Typography } from 'antd'
+import { useEffect, useState } from 'react'
+import { KFButton } from '../components/KFButton'
 const KFSDK = require("@kissflow/lowcode-client-sdk")
 
 export function CheckboxComponent() {
@@ -9,17 +10,49 @@ export function CheckboxComponent() {
     useEffect(() => {
         (async () => {
             await KFSDK.initialize();
-            console.log("KFSDK : " , await KFSDK?.app.getVariable("checkbox_enabled"))
+            console.log("KFSDK : ", await KFSDK?.app.getVariable("checkbox_enabled"))
         })()
     }, [])
-
-    // useEffect(() => {
-    //     KFSDK.app.page.setVariable({
-    //         checkbox_enabled: checked
-    //     })
-    // },[checked])
-
     return (
-        <Checkbox checked={checked} onChange={(e) => setChecked(e.target.checked)} />
+        <div style={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            // backgroundColor: "red",
+            height: "100vh"
+        }} >
+            <div style={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+                justifyContent: "space-around",
+                height: "100%"
+            }} >
+                <div
+                    style={{
+                        display: "flex",
+                    }}
+                >
+                    <Checkbox checked={checked} onChange={(e) => setChecked(e.target.checked)} />
+                    <Typography style={{ marginLeft: 8, fontSize: 15 }} >
+                        Read and Accept
+                        <a>&nbsp;Terms & Conditions</a>
+                    </Typography>
+                </div>
+                <div>
+                    <KFButton
+                        buttonType='primary'
+                        style={{ backgroundColor: "red", marginRight: 10, fontWeight: "600" }}
+                        className=""
+                    >Reject Invite</KFButton>
+                    <KFButton
+                        buttonType="primary"
+                        disabled={!checked}
+                        style={{ backgroundColor: "green", color: "white", fontWeight: "600" }}
+                        className=""
+                    >Accept Invite</KFButton>
+                </div>
+            </div>
+        </div>
     )
 }
