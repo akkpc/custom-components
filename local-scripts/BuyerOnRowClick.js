88a869dbda986bd09f79304e55c6f1b85e9bc666
Aanandakrishnan D <aanand@kissflow.com>
2024-03-14 18:05:54 +0530

    Updated local scripts

--- local-scripts/BuyerOnRowClick.js
+++ local-scripts/BuyerOnRowClick.js
@@ -1,7 +1,6 @@
 const _id = kf.eventParameters._id
 
 var SourcingDetails = await kf.api(`/process/2/${kf.account._id}/admin/Sourcing_Master_A00/${_id}`);
-
 console.log("SourcingDetails", SourcingDetails)
 const {
   Current_Stage,
@@ -16,114 +15,129 @@ const {
   RFP_End_Date,
   RFQ_End_Date,
   _created_by,
-  _last_completed_step: _activity_instance_id,
+  _current_context,
+  _last_completed_step,
   Event_Name,
   Event_Short_Description,
-  Category
+  Category,
+  _current_step,
+  _status
 } = SourcingDetails;
 
-let event_status = "event_creation_draft";
-let stepperObj = {
-  event_creation_draft: "",
-  assess_award: convertStringToDate(Awarding_Communication),
-  post_award_actions: "",
-  completed: ""
-}
+if (_status == "Draft") {
+  kf.app.openPage("buyer_sourcing_form_A03", { id: kf.eventParameters._id, aid: kf.eventParameters._activity_instance_id })
+} else {
 
-if (Event_Type.includes("RFI")) {
-  stepperObj.awaiting_rfi_response = convertStringToDate(RFI_End_Date)
-  event_status = "awaiting_rfi_response";
-  if (getDate(RFI_End_Date).getTime() < new Date().getTime()) {
-    stepperObj.rfi_evaluation_qualification = "";
-    event_status = "rfi_evaluation_qualification";
+  let event_status = "event_creation_draft";
+  let stepperObj = {
+    event_creation_draft: "",
+    assess_award: convertStringToDate(Awarding_Communication),
+    post_award_actions: "",
+    completed: ""
   }
-}
 
-if (Event_Type.includes("RFP")) {
-  stepperObj.rfp_configuration = convertStringToDate(RFP_Start_Date)
-  stepperObj.awaiting_rfp_response = convertStringToDate(RFP_End_Date)
-  stepperObj.rfp_technical_evaluation = ""
-
-  if (Current_Stage == "RFP") {
-    if (Event_Type.includes("RFI")) {
-      event_status = "rfp_configuration";
-    } else {
-      event_status = "awaiting_rfp_response";
-    }
-    if (getDate(RFP_End_Date) < new Date().getTime()) {
-      event_status = "rfp_technical_evaluation";
+  if (Event_Type.includes("RFI")) {
+    stepperObj.awaiting_rfi_response = convertStringToDate(RFI_End_Date)
+    event_status = "awaiting_rfi_response";
+    if (getDate(RFI_End_Date).getTime() < new Date().getTime()) {
+      stepperObj.rfi_evaluation_qualification = "";
+      event_status = "rfi_evaluation_qualification";
     }
   }
-}
 
-if (Event_Type.includes("RFQ")) {
-  stepperObj.rfq_configuration = convertStringToDate(RFQ_Start_Date)
-  stepperObj.awaiting_rfq_response = convertStringToDate(RFQ_End_Date)
-  stepperObj.rfq_commercial_evaluation = ""
+  if (Event_Type.includes("RFP")) {
+    stepperObj.rfp_configuration = convertStringToDate(RFP_Start_Date)
+    stepperObj.awaiting_rfp_response = convertStringToDate(RFP_End_Date)
+    stepperObj.rfp_technical_evaluation = ""
+
+    if (Current_Stage == "RFP") {
+      if (Event_Type.includes("RFI")) {
+        event_status = "rfp_configuration";
+      } else {
+        event_status = "awaiting_rfp_response";
+      }
+      if (getDate(RFP_End_Date) < new Date().getTime()) {
+        event_status = "rfp_technical_evaluation";
+      }
+    }
+  }
+
+  if (Event_Type.includes("RFQ")) {
+    stepperObj.rfq_configuration = convertStringToDate(RFQ_Start_Date)
+    stepperObj.awaiting_rfq_response = convertStringToDate(RFQ_End_Date)
+    stepperObj.rfq_commercial_evaluation = ""
+    if (Current_Stage == "RFQ") {
+      if (Event_Type.includes("RFP")) {
+        event_status = "rfq_configuration";
+      } else {
+        event_status = "awaiting_rfq_response";
+      }
+      if (getDate(RFQ_End_Date) < new Date().getTime()) {
+        event_status = "rfq_commercial_evaluation";
+      }
+    }
+  }
+
   if (Current_Stage == "RFQ") {
-    if (Event_Type.includes("RFP")) {
-      event_status = "rfq_configuration";
-    } else {
+    event_status = "rfq_configuration";
+    if (getDate(RFQ_Start_Date) < new Date().getTime()) {
       event_status = "awaiting_rfq_response";
     }
     if (getDate(RFQ_End_Date) < new Date().getTime()) {
       event_status = "rfq_commercial_evaluation";
     }
   }
-}
 
-if (Current_Stage == "RFQ") {
-  event_status = "rfq_configuration";
-  if (getDate(RFQ_Start_Date) < new Date().getTime()) {
-    event_status = "awaiting_rfq_response";
+  const availableTabs = [
+    {
+      key: "Summary",
+      componentId: "Container_DMRyxcgut",
+      name: "Summary",
+      hideComponents: []
+    },
+    {
+      key: "Event",
+      componentId: "Container_rHVp1xWj8",
+      name: "Event",
+      hideComponents: []
+    },
+    {
+      key: "Supplier",
+      componentId: "Container_XQUsOfW1X",
+      name: "Supplier",
+      hideComponents: []
+    },
+    {
+      key: "QnA",
+      componentId: "Container_HMbQnrRvH",
+      name: "QnA",
+      hideComponents: []
+    }
+  ]
+
+  let _activity_instance_id = _current_step.includes("Evaluation") ? _last_completed_step :  _current_context[0]._context_activity_instance_id
+  const payload = {
+    id: _id,
+    aid: _activity_instance_id,
+    timer_date: getDate(SourcingDetails[`${Current_Stage}_End_Date`]),
+    event_owner_name: _created_by.Name,
+    event_owner_email: _created_by.Email,
+    current_stage: Current_Stage,
+    current_status: _current_step,
+    category: Category,
+    locale_event_end_date: convertStringToDate(SourcingDetails[`${Current_Stage}_End_Date`]),
+    availableTabs: JSON.stringify(availableTabs),
+    user_type: "buyer",
+    event_name: Event_Name,
+    event_description: Event_Short_Description,
+    eventNumber: Event_Number,
+    sourcing_event_id: _id,
+    stepper: JSON.stringify(stepperObj)
   }
-  if (getDate(RFQ_End_Date) < new Date().getTime()) {
-    event_status = "rfq_commercial_evaluation";
-  }
+  console.log("stepperObj : ", payload)
+  kf.app.openPage("Sourcing_Buyer_Dashboard_A01", payload)
+
 }
-
-const availableTabs = [
-  {
-    key: "Summary",
-    componentId: "Container_DMRyxcgut",
-    name: "Summary",
-    hideComponents: []
-  },
-  {
-    key: "Event",
-    componentId: "Container_rHVp1xWj8",
-    name: "Event",
-    hideComponents: []
-  },
-  {
-    key: "QnA",
-    componentId: "Container_HMbQnrRvH",
-    name: "QnA",
-    hideComponents: []
-  }
-]
-
-
-const payload = {
-  id: _id,
-  aid: _activity_instance_id,
-  timer_date: getDate(SourcingDetails[`${Current_Stage}_End_Date`]),
-  event_owner_name: _created_by.Name,
-  event_owner_email: _created_by.Email,
-  current_stage: Current_Stage,
-  current_status: event_status,
-  category: Category,
-  locale_event_end_date: convertStringToDate(SourcingDetails[`${Current_Stage}_End_Date`]),
-  availableTabs: JSON.stringify(availableTabs),
-  user_type: "buyer",
-  event_name: Event_Name,
-  event_description: Event_Short_Description,
-  eventNumber: Event_Number,
-  sourcing_event_id: _id,
-  stepper: JSON.stringify(stepperObj)
-}
-console.log("stepperObj : ", payload)
-kf.app.openPage("Sourcing_Buyer_Dashboard_A01", payload)
 
 function convertStringToDate(dateString) {
   if (dateString) {
