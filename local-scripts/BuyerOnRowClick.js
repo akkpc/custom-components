e47f3ee6eabd5450779975ed685e988fcd18cf6d
Aanandakrishnan D <aanand@kissflow.com>
2024-04-08 15:01:00 +0530

    Fixed issue in timer

--- local-scripts/BuyerOnRowClick.js
+++ local-scripts/BuyerOnRowClick.js
@@ -26,99 +26,96 @@ const {
   Freeze_Award
 } = SourcingDetails;
 
-if (_status == "Draft") {
-  kf.app.openPage("buyer_sourcing_form_A03", { id: kf.eventParameters._id, aid: kf.eventParameters._activity_instance_id })
-} else {
-
-  let event_status = "event_creation_draft";
-  let stepperObj = {
-    event_creation_draft: "",
-    assess_award: convertStringToDate(Awarding_Communication),
-    post_award_actions: "",
-    completed: ""
-  }
-
-  if (Event_Type.includes("RFI")) {
-    stepperObj.awaiting_rfi_response = convertStringToDate(RFI_End_Date)
-    event_status = "awaiting_rfi_response";
-    if (getDate(RFI_End_Date).getTime() < new Date().getTime()) {
-      stepperObj.rfi_evaluation_qualification = "";
-      event_status = "rfi_evaluation_qualification";
-    }
-  }
-
-  if (Event_Type.includes("RFP")) {
-    // stepperObj.rfp_configuration = convertStringToDate(RFP_Start_Date)
-    stepperObj.awaiting_rfp_response = convertStringToDate(RFP_End_Date)
-    stepperObj.rfp_technical_evaluation = ""
-
-    if (Current_Stage == "RFP") {
-      // if (Event_Type.includes("RFI")) {
-      //   event_status = "rfp_configuration";
-      // } else {
-      //   event_status = "awaiting_rfp_response";
-      // }
-      event_status = "awaiting_rfp_response";
-      if (getDate(RFP_End_Date) < new Date().getTime()) {
-        event_status = "rfp_technical_evaluation";
-      }
-    }
-  }
-
-  if (Event_Type.includes("RFQ")) {
-    stepperObj.rfq_configuration = convertStringToDate(RFQ_Start_Date)
-    stepperObj.awaiting_rfq_response = convertStringToDate(RFQ_End_Date)
-    stepperObj.rfq_commercial_evaluation = ""
-    if (Current_Stage == "RFQ") {
-      // if (Event_Type.includes("RFP")) {
-      //   event_status = "rfq_configuration";
-      // } else {
-      //   event_status = "awaiting_rfq_response";
-      // }
-      event_status = "awaiting_rfq_response";
-      if (getDate(RFQ_End_Date) < new Date().getTime()) {
-        event_status = "rfq_commercial_evaluation";
-      }
-    }
-  }
-
-  const payload = {
-    id: _id,
-    aid: _current_context[0]._context_activity_instance_id,
-    timer_date: getDate(SourcingDetails[`${Current_Stage}_End_Date`]),
-    event_owner_name: _created_by.Name,
-    event_owner_email: _created_by.Email,
-    current_stage: Current_Stage,
-    current_status: encodeURIComponent(_current_step),
-    category: Category,
-    locale_event_end_date: convertStringToDate(SourcingDetails[`${Current_Stage}_End_Date`]),
-    user_type: "buyer",
-    event_name: Event_Name,
-    event_description: Event_Short_Description,
-    eventNumber: Event_Number,
-    sourcing_event_id: _id,
-    stepper: JSON.stringify(stepperObj),
-    end_date: SourcingDetails[`${Current_Stage}_End_Date`]
-  }
-
-  let pageId = "Sourcing_Buyer_Awaiting_Response_A01";
-
-  if (_current_step.includes("Evaluation")) {
-    let isEvaluator = _current_assigned_to.findIndex(({ _id }) => _id == kf.user._id);
-    if (isEvaluator >= 0) {
-      pageId = "Sourcing_Buyer_Evaluation_A00";
-    } else {
-      payload.aid = _last_completed_step;
-    }
-  } else if (_current_step == "Assess & Award") {
-    pageId = "Sourcing_Buyer_Assess_Award_A00";
-  }
-
-  console.log("stepperObj : ", payload)
-  await kf.app.setVariable("sourcing_custom_tab_key", "Summary")
-
-  kf.app.openPage(pageId, payload)
+let event_status = "event_creation_draft";
+let stepperObj = {
+  event_creation_draft: "",
+  assess_award: convertStringToDate(Awarding_Communication),
+  post_award_actions: "",
+  completed: ""
 }
+
+if (Event_Type.includes("RFI")) {
+  stepperObj.awaiting_rfi_response = convertStringToDate(RFI_End_Date)
+  event_status = "awaiting_rfi_response";
+  if (getDate(RFI_End_Date).getTime() < new Date().getTime()) {
+    stepperObj.rfi_evaluation_qualification = "";
+    event_status = "rfi_evaluation_qualification";
+  }
+}
+
+if (Event_Type.includes("RFP")) {
+  // stepperObj.rfp_configuration = convertStringToDate(RFP_Start_Date)
+  stepperObj.awaiting_rfp_response = convertStringToDate(RFP_End_Date)
+  stepperObj.rfp_technical_evaluation = ""
+
+  if (Current_Stage == "RFP") {
+    // if (Event_Type.includes("RFI")) {
+    //   event_status = "rfp_configuration";
+    // } else {
+    //   event_status = "awaiting_rfp_response";
+    // }
+    event_status = "awaiting_rfp_response";
+    if (getDate(RFP_End_Date) < new Date().getTime()) {
+      event_status = "rfp_technical_evaluation";
+    }
+  }
+}
+
+if (Event_Type.includes("RFQ")) {
+  stepperObj.rfq_configuration = convertStringToDate(RFQ_Start_Date)
+  stepperObj.awaiting_rfq_response = convertStringToDate(RFQ_End_Date)
+  stepperObj.rfq_commercial_evaluation = ""
+  if (Current_Stage == "RFQ") {
+    // if (Event_Type.includes("RFP")) {
+    //   event_status = "rfq_configuration";
+    // } else {
+    //   event_status = "awaiting_rfq_response";
+    // }
+    event_status = "awaiting_rfq_response";
+    if (getDate(RFQ_End_Date) < new Date().getTime()) {
+      event_status = "rfq_commercial_evaluation";
+    }
+  }
+}
+
+const payload = {
+  id: _id,
+  aid: _current_context[0]._context_activity_instance_id,
+  timer_date: getDate(SourcingDetails[`${Current_Stage}_End_Date`]),
+  event_owner_name: _created_by.Name,
+  event_owner_email: _created_by.Email,
+  current_stage: Current_Stage,
+  current_status: encodeURIComponent(_current_step),
+  category: Category,
+  locale_event_end_date: convertStringToDate(SourcingDetails[`${Current_Stage}_End_Date`]),
+  user_type: "buyer",
+  event_name: Event_Name,
+  event_description: Event_Short_Description,
+  eventNumber: Event_Number,
+  sourcing_event_id: _id,
+  stepper: JSON.stringify(stepperObj),
+  end_date: SourcingDetails[`${Current_Stage}_End_Date`],
+  timerSize: "big"
+}
+
+let pageId = "Sourcing_Buyer_Awaiting_Response_A01";
+
+if (_current_step.includes("Evaluation")) {
+  let isEvaluator = _current_assigned_to.findIndex(({ _id }) => _id == kf.user._id);
+  if (isEvaluator >= 0) {
+    pageId = "Sourcing_Buyer_Evaluation_A00";
+  } else {
+    payload.aid = _last_completed_step;
+  }
+} else if (_current_step == "Assess & Award") {
+  pageId = "Sourcing_Buyer_Assess_Award_A00";
+}
+
+console.log("stepperObj : ", payload)
+await kf.app.setVariable("sourcing_custom_tab_key", "Summary")
+
+kf.app.openPage(pageId, payload)
+
 
 function convertStringToDate(dateString) {
   if (dateString) {
